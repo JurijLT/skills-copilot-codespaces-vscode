f6d1e0819ab4f26808e912d8c35757b662c7df85
JurijLT <125045053+JurijLT@users.noreply.github.com>
2023-08-08 15:39:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberLink = document.getElementById("memberLink");
+    var skillsLink = document.getElementById("skillsLink");
+    var contactLink = document.getElementById("contactLink");
+    var aboutLink = document.getElementById("aboutLink");
+    member.style.display = "block";
+    skills.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    memberLink.style.color = "#fff";
+    skillsLink.style.color = "#000";
+    contactLink.style.color = "#000";
+    aboutLink.style.color = "#000";
+}
